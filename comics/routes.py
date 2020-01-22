a550d12c828cbb63ef64d925187a7056923ba312
skimanikings <kimanisimon856@gmail.com>
2020-01-22 08:42:54 +0300

    extends base html to home html

--- comics/routes.py
+++ comics/routes.py
@@ -1,1 +1,7 @@
 from flask import render_template, url_for
+from comics import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
---
+++ run.py
@@ -0,0 +1,3 @@
+from comics import app
+if __name__ == "__main__":   
+     app.run(debug=True)
