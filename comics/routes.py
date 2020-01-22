62c49d72563c49fe1db17341c9db7d73ade6b65f
dracangelo <kelvinmbuguaw@gmail.com>
2020-01-22 11:06:27 +0300

    created a registration form

--- comics/routes.py
+++ comics/routes.py
@@ -1,7 +1,20 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
 from comics import app
+from comics.forms import RegistrationForm
+
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 from comics import app
+
 if __name__ == "__main__":   
      app.run(debug=True)
