936d112829ee97678040d7a32f7bb0e67d7d932c
skimanikings <kimanisimon856@gmail.com>
2020-01-22 10:38:13 +0300

    create a module class for the api of superheroes

---
+++ comics/models.py
@@ -0,0 +1,10 @@
+
+
+
+class Superhero:
+    def __init__ (self,image,biography,powerstats,connections):
+        self.image = image
+        self.biography = biography
+        self.powerstats = powerstats
+        self.connection = connections
+        
