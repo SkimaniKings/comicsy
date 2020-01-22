6f5e75148e09081796cbbcc85daf610522d804d1
Abdi <oxabdi75@gmail.com>
2020-01-22 10:25:56 +0300

    added login credntial

--- comics/routes.py
+++ comics/routes.py
@@ -1,7 +1,21 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect, request
 from comics import app
+from comics.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
