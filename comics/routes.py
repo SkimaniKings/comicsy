420b59d094730e460b9624ca26cec4ce2bf7d7de
dracangelo <kelvinmbuguaw@gmail.com>
2020-01-23 08:00:49 +0300

    addeda registration form

--- comics/routes.py
+++ comics/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for, flash, redirect
-from comics import app
+from comics import app, bcrypt, db
 from comics.forms import RegistrationForm
+from comics.models import User
 
 
 
@@ -13,8 +14,14 @@ def home():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
